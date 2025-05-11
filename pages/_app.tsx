03d35ee704ccd34ec4539aba7ccdfe521b228578
Methmina Binara <binaraepa@gmail.com>
2025-05-12 00:42:22 +0530

    WIP

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,14 +1,6 @@
 import '@/styles/globals.css';
 import type { AppProps } from 'next/app';
-import Header from '@/components/Header';
 
 export default function App({ Component, pageProps }: AppProps) {
-  return (
-    <>
-      <Header />
-      <main className="">
-        <Component {...pageProps} />
-      </main>
-    </>
-  );
+  return <Component {...pageProps} />;
 }
